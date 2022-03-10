d7ff58ab28e639cb4a03395833c2e25fdd74ca86
AbishaiMarkGundy <85885719+AbishaiMarkGundy@users.noreply.github.com>
2022-03-10 13:04:00 -0600

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,7 @@ const About = () => {
       )}
 
       {role && <h2 className='about__role'>A {role}.</h2>}
-      <p className='about__desc'>{description && description}</p>
+      <p className='about__desc' style="text-align:center;">{description && description}</p>
 
       <div className='about__contact center'>
         {resume && (
